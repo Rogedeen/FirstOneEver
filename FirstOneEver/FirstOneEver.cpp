eaa2a5fc9bba20e2419c4cb07bda6c10e6a97129
Rogedeen <ynsisikgoz@gmail.com>
2025-05-14 20:50:09 +0300

    karakter hareketi

--- FirstOneEver/FirstOneEver.cpp
+++ FirstOneEver/FirstOneEver.cpp
@@ -1,37 +1,34 @@
 #include "raylib.h"
 #include <iostream>
-#include <stdlib.h>
+#include <vector>
+#include <string>
+#include <cstdlib>
 
-int main()
-{
-	const int screenWidth = 450;
-	const int screenHeight = 800;
+int main() {
+    const int screenWidth = 450;
+    const int screenHeight = 800;
 
-	InitWindow(screenWidth, screenHeight, "prototip");
-	SetTargetFPS(60);
-	
-	float backgroundY = 0;
-	const float scrollSpeed = 2.0f;
+    InitWindow(screenWidth, screenHeight, "Dikey Shooter Oyunu");
+    SetTargetFPS(60);
 
+    
+    Vector2 playerPosition = { screenWidth / 2 - 25, screenHeight - 100 };
+    const float playerSpeed = 5.0f; 
 
-	while (!WindowShouldClose())
-	{
-		backgroundY += scrollSpeed;
-		if (backgroundY >= screenHeight) backgroundY = 0;
-		
-		BeginDrawing();
-		ClearBackground(BLACK);
+    while (!WindowShouldClose()) {
+        
+        if (IsKeyDown(KEY_LEFT) && playerPosition.x > 0) playerPosition.x -= playerSpeed;
+        if (IsKeyDown(KEY_RIGHT) && playerPosition.x < screenWidth - 50) playerPosition.x += playerSpeed;
 
-		DrawRectangle(0, backgroundY, screenWidth, screenHeight, DARKGRAY);
-		DrawRectangle(0, backgroundY - screenHeight, screenWidth, screenHeight, DARKGRAY);
+        BeginDrawing();
+        ClearBackground(BLACK);
 
+        
+        DrawRectangleV(playerPosition, { 50, 50 }, BLUE);
 
-		EndDrawing();
-		//push 
-		//aananananananananananananan
-	}
+        EndDrawing();
+    }
 
-	CloseWindow();
-	return 0;	
-
+    CloseWindow();
+    return 0;
 }
